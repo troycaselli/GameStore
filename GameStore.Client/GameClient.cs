7a59abc85761129a567e02316f9373a092242f86
troycaselli <swordandtrowel1689@gmail.com>
2024-02-20 13:52:08 -0800

    enabled game editing capability

--- GameStore.Client/GameClient.cs
+++ GameStore.Client/GameClient.cs
@@ -42,4 +42,18 @@ public static class GameClient
         game.Id = games.Max(game => game.Id) + 1;
         games.Add(game);
     }
+
+    public static Game GetGame(int id)
+    {
+        return games.Find(game => game.Id == id) ?? throw new Exception($"Game with id: {id} could not be found");
+    }
+
+    public static void UpdateGame(Game updatedGame)
+    {
+        Game existingGame = GetGame(updatedGame.Id);
+        existingGame.Name = updatedGame.Name;
+        existingGame.Genre = updatedGame.Genre;
+        existingGame.Price = updatedGame.Price;
+        existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    }
 }
