a6f9fd49fe85accec81d916f3dfb92365788f909
troycaselli <swordandtrowel1689@gmail.com>
2024-02-22 15:14:59 -0800

    created get all endpoint

--- GameStore.Server/Program.cs
+++ GameStore.Server/Program.cs
@@ -1,6 +1,37 @@
+using GameStore.Server.Models;
+
+List<Game> games = new()
+{
+    new Game()
+    {
+        Id = 1,
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991, 2, 1)
+    },
+    new Game()
+    {
+        Id = 2,
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 9, 30)
+    },
+    new Game()
+    {
+        Id = 3,
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 9, 27)
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
