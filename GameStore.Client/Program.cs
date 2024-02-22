07d7668787205d0de1c18310309d054ecd54b7df
troycaselli <swordandtrowel1689@gmail.com>
2024-02-22 14:54:52 -0800

    created shared app build instructions

--- GameStore.Client/Program.cs
+++ GameStore.Client/Program.cs
@@ -1,3 +1,17 @@
+// using Microsoft.AspNetCore.Components.Web;
+// using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+// using GameStore.Client;
+
+// var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// builder.RootComponents.Add<App>("#app");
+// builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// await builder.Build().RunAsync();
+
+
+// Alternate app initialization for hot reloading with SignalR
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.SignalR.Client; // Import SignalR client namespace
